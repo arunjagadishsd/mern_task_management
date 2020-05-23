4a21b37db1af034849052e79689d1879d319e99c
aj <arunjagadishsd@gmail.com>
2020-05-23 19:48:08 +0530

    Added status and label to todo model

--- server/models/todo.model.js
+++ server/models/todo.model.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
-const { Schema, model } = mongoose
+const {
+  Schema,
+  model
+} = mongoose
 
 const todoSchema = new Schema({
-  text: String,
-  due: Date
+  text: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: Schema.Types.ObjectId,
+    ref: 'Label'
+  },
+  status: {
+    type: Schema.Types.ObjectId,
+    ref: 'Status'
+
+  },
+  priority: {
+    type: String,
+    required: true,
+    enum: ['P1', 'P2', 'P3', 'P4'],
+  },
+  due_date: {
+    type: Date
+  },
+  created_date: {
+    type: Date
+  }
+
 })
-module.exports = Todo = mongoose.model('todo', todoSchema)
+module.exports = Todo = model('Todo', todoSchema)
